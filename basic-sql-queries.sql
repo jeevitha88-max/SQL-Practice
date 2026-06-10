31eda7a78d6fef98f45d0276345fccd9dfa96add
Jeevitha Sivaperumal <jeevithasivageetha@gmail.com>
2026-06-10 13:25:51 +0530

    Create basic-sql-queries.sql

---
+++ basic-sql-queries.sql
@@ -0,0 +1,11 @@
+SELECT * FROM Employees;
+
+SELECT DISTINCT Department
+FROM Employees;
+
+SELECT Name, Salary
+FROM Employees
+WHERE Salary > 50000;
+
+SELECT * FROM Employees
+ORDER BY Salary DESC;
